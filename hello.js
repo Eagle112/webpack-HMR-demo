0534861e7ede6c404ac1082a0e35cb7fbd18cd44
yinghongbin <yinghongbin@appledeMacBook-Pro.local>
2021-04-20 15:13:54 +0800

    123

--- hello.js
+++ hello.js
@@ -1,2 +1,2 @@
-const hello = () => 'hello world nice'
+const hello = () => 'hello '
 export default hello
--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import hello from './hello.js'
 const div = document.createElement('div')
 div.innerHTML = hello()
 
+console.log(1232)
 document.body.appendChild(div)
+if(module.hot) {
+  module.hot.accept(['./hello.js','./index.js'], function() {
+      // div.innerHTML = hello()
+  })
+}
